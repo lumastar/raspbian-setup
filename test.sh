3fea8cc04e101dd49eb2745a51836e4201acd154
William Squires <wil@edwar.do>
2018-11-29 12:02:15 +0000

    Update script tests to be more comprehensive

--- test.sh
+++ test.sh
@@ -2,16 +2,40 @@
 
 set -e
 
+echo "Will now test raspbian-setup scripts"
 cd /raspbian-setup
 
 # Test silent_boot.sh
+echo "Will now test silent boot script"
 ./silent_boot.sh enable
 ./silent_boot.sh disable
 
 # Test update_user.sh
-./update_user.sh pi test test
-ls /home
+echo "Will now test update user script"
+USER_NAME=test1
+USER_PASSWORD=test1
+echo "Will change pi user name, group and password to $"
+./update_user.sh pi $USER_NAME $USER_PASSWORD
+# Try running whoami as the changed user
+su $USER_NAME <<EOSU
+$USER_PASSWORD
+whoami
+EOSU
+# Check the users group has been changed
+if ! $(id -Gn $USER_NAME | grep -qw $USER_NAME); then
+	echo "Update user script failed to change group"
+	exit 1
+fi
 
 # Test set_hostname.sh
-./set_hostname.sh hostname
-cat /etc/hostname
+echo "Will now test set hostname script"
+CURRENT_HOSTNAME=$(cat /etc/hostname)
+echo "CURRENT_HOSTNAME: $CURRENT_HOSTNAME"
+NEW_HOSTNAME=raspbian-setup-test
+echo "NEW HOSTNAME: $NEW_HOSTNAME"
+./set_hostname.sh $NEW_HOSTNAME
+CURRENT_HOSTNAME=$(cat /etc/hostname)
+if [ $NEW_HOSTNAME != $CURRENT_HOSTNAME ]; then
+	echo "Set hostname failed!"
+	exit 1
+]
