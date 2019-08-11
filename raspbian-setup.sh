e43b1b77f09b0c7e5df5fbe357c21e0d8c876ac1
wwwil <wwwil.squires@gmail.com>
2019-08-11 13:28:00 +0100

    Remove logging to file

--- raspbian-setup.sh
+++ raspbian-setup.sh
@@ -7,23 +7,18 @@ set -o xtrace
 
 CONFIG_PATH=$1
 
-# Create a log file
-LOG_PATH=$2
-echo "raspbian-setup.sh" > $LOG_PATH
-date +"%Y-%m-%d-%T" > $LOG_PATH
-
 while read line; do
 	IFS="=" read -a lineparts <<<"$line"
 	case "${lineparts[0]}" in
 		SILENT_BOOT)
-			./silent-boot.sh "${lineparts[1]}" > $LOG_PATH
+			./silent-boot.sh "${lineparts[1]}"
 			;;
 		HOSTNAME)
-			./set-hostname.sh "${lineparts[1]}" > $LOG_PATH
+			./set-hostname.sh "${lineparts[1]}"
 			;;
 		UPDATE_USER)
 			IFS="," read -a argparts <<<"${lineparts[1]}"
-			./update-user.sh "${argparts[0]}" "${argparts[1]}" "${argparts[2]}" > $LOG_PATH
+			./update-user.sh "${argparts[0]}" "${argparts[1]}" "${argparts[2]}"
 			;;
 		INSTALL_WIREGUARD)
 			if [ "${lineparts[1]}" == "true" ]; then
